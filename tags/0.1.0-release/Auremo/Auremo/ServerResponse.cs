191f60dfe056bc51b28bdfd8150443884ed014dd
auremompd <auremompd@aed9fa71-8930-dd05-51b1-77e14e162102>
2012-08-02 15:36:30 +0000

    Tag the 0.1.0 release.

---
+++ tags/0.1.0-release/Auremo/Auremo/ServerResponse.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Copyright 2012 Mikko Teräs
+ *
+ * This file is part of Auremo.
+ *
+ * Auremo is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 2.
+ *
+ * Auremo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Auremo. If not, see http://www.gnu.org/licenses/.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    class ServerResponse
+    {
+        private IList<ServerResponseLine> m_ResponseLines = null;
+        private ServerResponseLine m_Status = null;
+
+        public ServerResponse()
+        {
+        }
+
+        public ServerResponse(IList<string> lines)
+        {
+            // TODO: destructive, could be cleaner.
+            if (lines.Count > 0)
+            {
+                m_Status = new ServerResponseLine(lines.Last());
+                lines.RemoveAt(lines.Count - 1);
+                m_ResponseLines = new List<ServerResponseLine>();
+
+                foreach (string line in lines)
+                {
+                    m_ResponseLines.Add(new ServerResponseLine(line));
+                }
+            }
+        }
+
+        public IList<ServerResponseLine> Lines
+        {
+            get
+            {
+                return m_ResponseLines;
+            }
+        }
+
+        public bool IsOK
+        {
+            get
+            {
+                return m_Status.Full.StartsWith("OK");
+            }
+        }
+
+        public bool IsACK
+        {
+            get
+            {
+                return m_Status.Full.StartsWith("ACK");
+            }
+        }
+
+        public ServerResponseLine Status
+        {
+            get
+            {
+                return m_Status;
+            }
+        }
+    }
+}
