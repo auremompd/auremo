94bb89bb7903889ec46d58b20c66791e8d68b152
auremompd <auremompd@aed9fa71-8930-dd05-51b1-77e14e162102>
2012-08-07 21:02:49 +0000

    Add view model classes for putting songs and paths into a tree view.

---
+++ Auremo/Auremo/DirectoryTreeViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    /// <summary>
+    /// Wraps a directory (aka folder) name so that it can be consumed by a
+    /// TreeView[Item].
+    /// </summary>
+    class DirectoryTreeViewModel : ITreeViewModel, INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        #endregion
+
+        string m_DirectoryName = "";
+        ITreeViewModel m_Parent = null;
+        IList<ITreeViewModel> m_Children = new ObservableCollection<ITreeViewModel>();
+        bool m_IsSelected = false;
+        bool m_IsExpanded = false;
+
+        public DirectoryTreeViewModel(string name, ITreeViewModel parent)
+        {
+            m_DirectoryName = name;
+            m_Parent = parent;
+        }
+
+        public string DisplayString
+        {
+            get
+            {
+                return m_DirectoryName;
+            }
+        }
+
+        public IList<ITreeViewModel> Children
+        {
+            get
+            {
+                return m_Children;
+            }
+        }
+
+        public bool IsSelected
+        {
+            get
+            {
+                return m_IsSelected;
+            }
+            set
+            {
+                if (value != m_IsSelected)
+                {
+                    m_IsSelected = value;
+                    NotifyPropertyChanged("IsSelected");
+                }
+            }
+        }
+
+        public bool IsExpanded
+        {
+            get
+            {
+                return m_IsExpanded;
+            }
+            set
+            {
+                if (value != m_IsExpanded)
+                {
+                    m_IsExpanded = value;
+
+                    if (m_IsExpanded && m_Parent != null)
+                    {
+                        m_Parent.IsExpanded = true;
+                    }
+
+                    NotifyPropertyChanged("IsExpanded");
+                }
+            }
+        }
+
+        public void AddChild(ITreeViewModel child)
+        {
+            m_Children.Add(child);
+            NotifyPropertyChanged("Children");
+        }
+
+        public override string ToString()
+        {
+            if (m_Parent == null)
+            {
+                return m_DirectoryName;
+            }
+            else
+            {
+                return m_Parent.ToString() + "/" + m_DirectoryName;
+            }
+        }
+    }
+}
---
+++ Auremo/Auremo/SongMetadataTreeViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    /// <summary>
+    /// Wraps a SongMetadata object so that it can be consumed by a TreeView[Item].
+    /// </summary>
+    class SongMetadataTreeViewModel : ITreeViewModel, INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        #endregion
+
+        private SongMetadata m_Song;
+        private ITreeViewModel m_Parent;
+        private bool m_IsSelected = false;
+
+        public SongMetadataTreeViewModel(SongMetadata song, ITreeViewModel parent)
+        {
+            m_Song = song;
+            m_Parent = parent;
+        }
+
+        public string DisplayString
+        {
+            get
+            {
+                return m_Song.Title;
+            }
+        }
+
+        public void AddChild(ITreeViewModel child)
+        {
+            throw new Exception("Attempt to add a child to a SongMetadataTreeViewModel.");
+        }
+
+        public IList<ITreeViewModel> Children
+        {
+            get
+            {
+                return new List<ITreeViewModel>(); // Can't have child nodes.
+            }
+        }
+
+        public bool IsSelected
+        {
+            get
+            {
+                return m_IsSelected;
+            }
+            set
+            {
+                if (value != m_IsSelected)
+                {
+                    m_IsSelected = value;
+                    NotifyPropertyChanged("IsSelected");
+                }
+            }
+        }
+
+        public bool IsExpanded
+        {
+            get
+            {
+                return false; // A song is a leaf node in any tree.
+            }
+            set
+            {
+                throw new Exception("Attempt to expand a SongMetadataTreeViewModel.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return m_Parent.ToString() + m_Song.Title;
+        }
+    }
+}
