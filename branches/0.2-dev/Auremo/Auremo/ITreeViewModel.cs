7c2c5cb639d3717101b6de10505325adb377889b
auremompd <auremompd@aed9fa71-8930-dd05-51b1-77e14e162102>
2012-08-07 21:02:49 +0000

    Add view model classes for putting songs and paths into a tree view.

---
+++ branches/0.2-dev/Auremo/Auremo/ITreeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    public interface ITreeViewModel
+    {
+        string DisplayString { get; }
+        void AddChild(ITreeViewModel child);
+        IList<ITreeViewModel> Children { get; }
+        bool IsSelected { get; set; }
+        bool IsExpanded { get; set; }
+    }
+}
