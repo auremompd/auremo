27671d1319c56de02b8695f4ffb28b076ed7311e
auremompd <auremompd@aed9fa71-8930-dd05-51b1-77e14e162102>
2012-08-15 22:17:30 +0000

    Tag release 0.2.0.

---
+++ tags/0.2.0-release/Auremo/ITreeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    public interface ITreeViewModel : IComparable
+    {
+        string DisplayString { get; }
+        void AddChild(ITreeViewModel child);
+        ITreeViewModel Parent { get; }
+        IList<ITreeViewModel> Children { get; }
+        bool IsSelected { get; set; }
+        bool IsExpanded { get; set; }
+        bool IsMultiSelected { get; set; }
+        TreeViewMultiSelection MultiSelection { get; }
+        int HierarchyID { get; set; }
+        void OnAncestorCollapsed();
+    }
+}
