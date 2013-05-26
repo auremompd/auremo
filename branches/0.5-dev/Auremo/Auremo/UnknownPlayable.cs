0fbd237aee3c1d4620c39cab6f66368a782e5966
auremompd <auremompd@aed9fa71-8930-dd05-51b1-77e14e162102>
2013-05-26 11:58:04 +0000

    Add UnknownPlayable class to fix playlist and status messages.

---
+++ branches/0.5-dev/Auremo/Auremo/UnknownPlayable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    public class UnknownPlayable : Playable
+    {
+        private UnknownPlayable()
+        {
+        }
+
+        public UnknownPlayable(string path)
+        {
+            Path = path;
+            Title = Utils.SplitPath(path).Item2;
+        }
+
+        public string Path
+        {
+            get;
+            private set;
+        }
+
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public string Artist
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public string Album
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public int? Year
+        {
+            get
+            {
+                return null;
+            }
+        }
+    }
+}
