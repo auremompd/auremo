85a7032b5d85316b592a5c5d0599847b27235183
auremompd@gmail.com <auremompd@gmail.com@aed9fa71-8930-dd05-51b1-77e14e162102>
2013-06-08 09:11:33 +0000

    Tag 0.5.pre3.

---
+++ tags/0.5.pre3/Auremo/Auremo/StreamMetadata.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    public class StreamMetadata : Playable, IComparable
+    {
+        public StreamMetadata()
+        {
+            Path = null;
+            Title = null;
+        }
+
+        public string Path
+        {
+            get;
+            set;
+        }
+
+        private string m_Title = null;
+
+        public string Title
+        {
+            get
+            {
+                if (m_Title == null)
+                {
+                    return Path;
+                }
+                else
+                {
+                    return m_Title;
+                }
+            }
+            set
+            {
+                m_Title = value;
+            }
+        }
+
+        public string Artist
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public string Album
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public int? Year
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public int CompareTo(object o)
+        {
+            if (o is StreamMetadata)
+            {
+                StreamMetadata rhs = (StreamMetadata)o;
+                return Path.ToLowerInvariant().CompareTo(rhs.Path.ToLowerInvariant());
+            }
+            else if (o is SongMetadata)
+            {
+                return 1;
+            }
+            else
+            {
+                throw new Exception("StreamMetadata: attempt to compare to an incompatible object");
+            }
+        }
+    }
+}
