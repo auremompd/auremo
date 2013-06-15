3120818f676d5f7b5adb6d49ff0df50b776f0de8
auremompd@gmail.com <auremompd@gmail.com@aed9fa71-8930-dd05-51b1-77e14e162102>
2013-06-15 13:51:16 +0000

    Tag 0.5.0-release.

---
+++ tags/0.5.0-release/Auremo/Auremo/NegateBoolConverter.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Copyright 2013 Mikko Teräs and Niilo Säämänen.
+ *
+ * This file is part of Auremo.
+ *
+ * Auremo is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 2.
+ *
+ * Auremo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Auremo. If not, see http://www.gnu.org/licenses/.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Auremo
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class NegateBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+    }
+}
---
+++ tags/0.5.0-release/Auremo/Auremo/UnknownPlayable.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    public class UnknownPlayable : Playable
+    {
+        private UnknownPlayable()
+        {
+        }
+
+        public UnknownPlayable(string path)
+        {
+            Path = path;
+
+            if (path.Contains("://"))
+            {
+                // Looks like a URL.
+                Title = path;
+            }
+            else
+            {
+                // Looks like a stream.
+                Title = Utils.SplitPath(path).Item2;
+            }
+        }
+
+        public string Path
+        {
+            get;
+            private set;
+        }
+
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public string Artist
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public string Album
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public int? Year
+        {
+            get
+            {
+                return null;
+            }
+        }
+    }
+}
