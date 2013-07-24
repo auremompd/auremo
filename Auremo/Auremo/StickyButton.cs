881fdbcf6b63206120e4ae04da246b36986f63e8
auremompd@gmail.com <auremompd@gmail.com@aed9fa71-8930-dd05-51b1-77e14e162102>
2013-07-24 17:25:36 +0000

    Remove NestedProperty, implement playback button glow with a custom control.

---
+++ Auremo/Auremo/StickyButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Auremo
+{
+    // A standard button with an extra property "IsDown" for buttons that have a
+    // server-dependent glow state.
+    public class StickyButton : Button
+    {
+        public bool IsDown
+        {
+            get
+            {
+                return (bool)GetValue(IsDownProperty);
+            }
+            set
+            {
+                SetValue(IsDownProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty IsDownProperty = DependencyProperty.Register("IsDown", typeof(bool), typeof(StickyButton), new UIPropertyMetadata(false));
+    }
+}
