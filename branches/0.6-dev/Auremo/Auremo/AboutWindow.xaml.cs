f94b51465197b216a217352503518c588b3b1cf1
auremompd@gmail.com <auremompd@gmail.com@aed9fa71-8930-dd05-51b1-77e14e162102>
2013-09-04 14:23:57 +0000

    Add new About window.

---
+++ branches/0.6-dev/Auremo/Auremo/AboutWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Auremo
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        private MainWindow m_Parent = null;
+
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        public AboutWindow(MainWindow parent)
+        {
+            InitializeComponent();
+
+            m_Parent = parent;
+            Version ver = Assembly.GetExecutingAssembly().GetName().Version;
+            m_VersionNumber.Text = "Version " + ver.Major + "." + ver.Minor + "." + ver.Build;
+                
+                //.GetExecutingAssembly().ImageRuntimeVersion;
+        }
+
+        private void OnCloseClicked(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OnClose(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            m_Parent.OnChildWindowClosing(this);
+        }
+    }
+}
