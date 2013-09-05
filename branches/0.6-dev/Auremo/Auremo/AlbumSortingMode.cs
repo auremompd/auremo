8d1190ba6670bd1010350bb9aa778841cc045cd0
auremompd@gmail.com <auremompd@gmail.com@aed9fa71-8930-dd05-51b1-77e14e162102>
2013-09-05 20:25:08 +0000

    Edit album sorting mode names slightly. Add a GUI widget for the album sort mode option.

---
+++ branches/0.6-dev/Auremo/Auremo/AlbumSortingMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    public enum AlbumSortingMode
+    {
+        ByDate = 0,
+        ByName = 1
+    }
+}
