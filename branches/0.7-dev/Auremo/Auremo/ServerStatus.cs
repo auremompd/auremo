f7fc053f2566b2c18b2ddd3094d19080d54c3b90
auremompd@gmail.com <auremompd@gmail.com@aed9fa71-8930-dd05-51b1-77e14e162102>
2013-09-22 19:42:41 +0000

    Branch /branches/0.7-dev from trunk.

---
+++ branches/0.7-dev/Auremo/Auremo/ServerStatus.cs
@@ -0,0 +1,500 @@
+﻿/*
+ * Copyright 2013 Mikko Teräs and Niilo Säämänen.
+ *
+ * This file is part of Auremo.
+ *
+ * Auremo is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 2.
+ *
+ * Auremo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Auremo. If not, see http://www.gnu.org/licenses/.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    public class ServerStatus : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        #endregion
+
+        private bool m_OK = false;
+        private int? m_Volume = null;
+        private int m_PlaylistVersion = -1;
+        private int m_CurrentSongIndex = -1;
+        private int m_PlayPosition = 0;
+        private int m_SongLength = 0;
+        private string m_State = "";
+        private int m_DatabaseUpdateTime = 0;
+
+        public ServerStatus()
+        {
+            Reset();
+        }
+
+        public void Update(ServerConnection connection)
+        {
+            ReadStatus(connection);
+            ReadStats(connection);
+        }
+
+        private void ReadStatus(ServerConnection connection)
+        {
+            ServerResponse response = null;
+            bool ok = false;
+
+            if (connection != null && connection.Status == ServerConnection.State.Connected)
+            {
+                response = Protocol.Status(connection);
+                ok = response != null && response.IsOK;
+            }
+
+            if (!ok && m_OK)
+            {
+                // The server disappeared.
+                Reset();
+            }
+
+            m_OK = ok;
+
+            if (m_OK)
+            {
+                // Not all the values checked for below are always
+                // present. Handle them specially so that defaults
+                // can be provided.
+                int currentSongIndex = -1;
+                int playPosition = 0;
+                int songLength = 0;
+                string audioQuality = "";
+                string errorMessage = "";
+
+                foreach (ServerResponseLine line in response.ResponseLines)
+                {
+                    if (line.Name == "state")
+                    {
+                        if (m_State != line.Value)
+                        {
+                            State = line.Value;
+                        }
+                    }
+                    else if (line.Name == "volume")
+                    {
+                        int? volume = Utils.StringToInt(line.Value);
+
+                        if (volume == null || volume.Value < 0 || volume > 100)
+                        {
+                            Volume = null;
+                        }
+                        else
+                        {
+                            Volume = volume;
+                        }
+                    }
+                    else if (line.Name == "playlist")
+                    {
+                        int? version = Utils.StringToInt(line.Value);
+                        PlaylistVersion = version.HasValue ? version.Value : -1;
+                    }
+                    else if (line.Name == "song")
+                    {
+                        int? index = Utils.StringToInt(line.Value);
+                        currentSongIndex = index.HasValue ? index.Value : -1;
+                    }
+                    else if (line.Name == "time")
+                    {
+                        string[] pieces = line.Value.Split(':');
+
+                        if (pieces.Length == 2)
+                        {
+                            int? position = Utils.StringToInt(pieces[0]);
+                            int? length = Utils.StringToInt(pieces[1]);
+
+                            if (position.HasValue && length.HasValue)
+                            {
+                                playPosition = position.Value;
+                                songLength = length.Value;
+                            }
+                        }
+                    }
+                    else if (line.Name == "random")
+                    {
+                        IsOnRandom = line.Value == "1";
+                    }
+                    else if (line.Name == "repeat")
+                    {
+                        IsOnRepeat = line.Value == "1";
+                    }
+                    else if (line.Name == "audio")
+                    {
+                        if (line.Value == "0:?:0")
+                        {
+                            // A little kludge to preserve the previous audio
+                            // quality text during song changes instead of
+                            // flashing "0 kHz ? bps 0 channels" for a moment.
+                            audioQuality = AudioQuality;
+                        }
+                        else
+                        {
+                            string[] parts = line.Value.Split(':');
+
+                            if (parts.Length == 3)
+                            {
+                                audioQuality = parts[0] + " kHz, " + parts[1] + " bits per sample, ";
+
+                                if (parts[2] == "1")
+                                {
+                                    audioQuality += "mono";
+                                }
+                                else if (parts[2] == "2")
+                                {
+                                    audioQuality += "stereo";
+                                }
+                                else
+                                {
+                                    audioQuality += parts[2] + " channels";
+                                }
+                            }
+                            else
+                            {
+                                AudioQuality = "";
+                            }
+                        }
+                    }
+                    else if (line.Name == "error")
+                    {
+                        errorMessage = "Error: " + line.Value;
+                    }
+                }
+
+                CurrentSongIndex = currentSongIndex;
+                PlayPosition = playPosition;
+                SongLength = songLength;
+                AudioQuality = audioQuality;
+                ErrorMessage = errorMessage;
+            }
+        }
+
+        private void ReadStats(ServerConnection connection)
+        {
+            ServerResponse response = null;
+            bool ok = false;
+
+            if (connection != null && connection.Status == ServerConnection.State.Connected)
+            {
+                response = Protocol.Stats(connection);
+                ok = response != null && response.IsOK;
+            }
+
+            if (!ok && m_OK)
+            {
+                // The server disappeared.
+                Reset();
+            }
+
+            m_OK = ok;
+
+            if (m_OK)
+            {
+                foreach (ServerResponseLine line in response.ResponseLines)
+                {
+                    if (line.Name == "db_update")
+                    {
+                        int? time = Utils.StringToInt(line.Value);
+                        DatabaseUpdateTime = time.HasValue ? time.Value : -1;
+                        return; // We're not interested in anything else right now.
+                    }
+                }
+            }
+        }
+
+        public bool OK
+        {
+            get
+            {
+                return m_OK;
+            }
+            private set
+            {
+                if (m_OK != value)
+                {
+                    m_OK = value;
+                    NotifyPropertyChanged("OK");
+                }
+            }
+        }
+
+        public string State
+        {
+            get
+            {
+                return m_State;
+            }
+            private set
+            {
+                if (m_State != value)
+                {
+                    m_State = value;
+                    IsPlaying = m_State == "play";
+                    IsPaused = m_State == "pause";
+                    IsStopped = m_State == "stop";
+                    NotifyPropertyChanged("State");
+                }
+            }
+        }
+
+        bool m_IsPlaying = false;
+        public bool IsPlaying
+        {
+            get
+            {
+                return m_IsPlaying;
+            }
+            private set
+            {
+                if (value != m_IsPlaying)
+                {
+                    m_IsPlaying = value;
+                    NotifyPropertyChanged("IsPlaying");
+                }
+            }
+        }
+
+        bool m_IsPaused = false;
+        public bool IsPaused
+        {
+            get
+            {
+                return m_IsPaused;
+            }
+            private set
+            {
+                if (value != m_IsPaused)
+                {
+                    m_IsPaused = value;
+                    NotifyPropertyChanged("IsPaused");
+                }
+            }
+        }
+
+        bool m_IsStopped = false;
+        public bool IsStopped
+        {
+            get
+            {
+                return m_IsStopped;
+            }
+            private set
+            {
+                if (value != m_IsStopped)
+                {
+                    m_IsStopped = value;
+                    NotifyPropertyChanged("IsStopped");
+                }
+            }
+        }
+
+        bool m_IsOnRepeat = false;
+        public bool IsOnRepeat
+        {
+            get
+            {
+                return m_IsOnRepeat;
+            }
+            private set
+            {
+                if (value != m_IsOnRepeat)
+                {
+                    m_IsOnRepeat = value;
+                    NotifyPropertyChanged("IsOnRepeat");
+                }
+            }
+        }
+
+        bool m_IsOnRandom = false;
+        public bool IsOnRandom
+        {
+            get
+            {
+                return m_IsOnRandom;
+            }
+            private set
+            {
+                if (value != m_IsOnRandom)
+                {
+                    m_IsOnRandom = value;
+                    NotifyPropertyChanged("IsOnRandom");
+                }
+            }
+        }
+
+        public int? Volume
+        {
+            get
+            {
+                return m_Volume;
+            }
+            private set
+            {
+                if (m_Volume != value)
+                {
+                    m_Volume = value;
+                    NotifyPropertyChanged("Volume");
+                }
+            }
+        }
+
+        public int PlaylistVersion
+        {
+            get
+            {
+                return m_PlaylistVersion;
+            }
+            private set
+            {
+                if (m_PlaylistVersion != value)
+                {
+                    m_PlaylistVersion = value;
+                    NotifyPropertyChanged("PlaylistVersion");
+                }
+            }
+        }
+
+        public int CurrentSongIndex
+        {
+            get
+            {
+                return m_CurrentSongIndex;
+            }
+            private set
+            {
+                if (m_CurrentSongIndex != value)
+                {
+                    m_CurrentSongIndex = value;
+                    NotifyPropertyChanged("CurrentSongIndex");
+                }
+            }
+        }
+
+        public int PlayPosition
+        {
+            get
+            {
+                return m_PlayPosition;
+            }
+            private set
+            {
+                if (m_PlayPosition != value)
+                {
+                    m_PlayPosition = value;
+                    NotifyPropertyChanged("PlayPosition");
+                }
+            }
+        }
+
+        public int SongLength
+        {
+            get
+            {
+                return m_SongLength;
+            }
+            private set
+            {
+                if (m_SongLength != value)
+                {
+                    m_SongLength = value;
+                    NotifyPropertyChanged("SongLength");
+                }
+            }
+        }
+
+        public int DatabaseUpdateTime
+        {
+            get
+            {
+                return m_DatabaseUpdateTime;
+            }
+            private set
+            {
+                if (value != m_DatabaseUpdateTime)
+                {
+                    m_DatabaseUpdateTime = value;
+                    NotifyPropertyChanged("DatabaseUpdateTime");
+                }
+            }
+        }
+
+        private string m_AudioQuality = "";
+        public string AudioQuality
+        {
+            get
+            {
+                return m_AudioQuality;
+            }
+            private set
+            {
+                if (value != m_AudioQuality)
+                {
+                    m_AudioQuality = value;
+                    NotifyPropertyChanged("AudioQuality");
+                }
+            }
+        }
+
+        private string m_ErrorMessage = "";
+        public string ErrorMessage
+        {
+            get
+            {
+                return m_ErrorMessage;
+            }
+            private set
+            {
+                if (value != m_ErrorMessage)
+                {
+                    m_ErrorMessage = value;
+                    NotifyPropertyChanged("ErrorMessage");
+                }
+            }
+        }
+
+        private void Reset()
+        {
+            OK = false;
+            Volume = null;
+            PlaylistVersion = -1;
+            CurrentSongIndex = -1;
+            PlayPosition = 0;
+            SongLength = 0;
+            IsPlaying = false;
+            IsPaused = false;
+            IsStopped = false;
+            IsOnRandom = false;
+            IsOnRepeat = false;
+            State = "";
+            DatabaseUpdateTime = 0;
+            AudioQuality = "";
+            ErrorMessage = "";
+        }
+    }
+}
---
+++ branches/0.7-dev/Auremo/Auremo/SongMetadata.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * Copyright 2013 Mikko Teräs and Niilo Säämänen.
+ *
+ * This file is part of Auremo.
+ *
+ * Auremo is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 2.
+ *
+ * Auremo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Auremo. If not, see http://www.gnu.org/licenses/.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    public class SongMetadata : Playable, IComparable
+    {
+        public SongMetadata()
+        {
+            Path = null;
+            Length = null;
+            Track = null;
+            Date = null;
+
+            Artist = "Unknown Artist";
+            Genre = "No Genre";
+            Album = "Unknown Album";
+        }
+
+        public string Path
+        {
+            get;
+            set;
+        }
+
+        private string m_Title = "";
+
+        public string Title
+        {
+            get
+            {
+                if (m_Title.Length > 0)
+                {
+                    return m_Title;
+                }
+                else
+                {
+                    return Utils.SplitPath(Path).Item2;
+                }
+            }
+            set
+            {
+                m_Title = value;
+            }
+        }
+
+        public string Artist
+        {
+            get;
+            set;
+        }
+
+        public string Album
+        {
+            get;
+            set;
+        }
+
+        public string Genre
+        {
+            get;
+            set;
+        }
+
+        public int? Length
+        {
+            get;
+            set;
+        }
+
+        public int? Track
+        {
+            get;
+            set;
+        }
+
+        public string Date
+        {
+            get;
+            set;
+        }
+
+        public string Year
+        {
+            get
+            {
+                return Utils.ExtractYearFromDateString(Date);
+            }
+        }
+
+        public override string ToString()
+        {
+            return Artist + ": " + Title + " (" + Album + ")";
+        }
+
+        public int CompareTo(object o)
+        {
+            if (o is SongMetadata)
+            {
+                SongMetadata rhs = (SongMetadata)o;
+                return StringComparer.Ordinal.Compare(Path, rhs.Path);
+            }
+            else if (o is StreamMetadata)
+            {
+                return -1;
+            }
+            else
+            {
+                throw new Exception("SongMetadata: attempt to compare to an incompatible object");
+            }
+        }
+    }
+}
