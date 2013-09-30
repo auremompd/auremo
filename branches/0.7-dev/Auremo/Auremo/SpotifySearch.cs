c41ba566d4651379317e5547d7caaff543e65172
auremompd@gmail.com <auremompd@gmail.com@aed9fa71-8930-dd05-51b1-77e14e162102>
2013-09-30 21:47:57 +0000

    Add missing file.

---
+++ branches/0.7-dev/Auremo/Auremo/SpotifySearch.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Copyright 2013 Mikko Teräs and Niilo Säämänen.
+ *
+ * This file is part of Auremo.
+ *
+ * Auremo is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 2.
+ *
+ * Auremo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Auremo. If not, see http://www.gnu.org/licenses/.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Auremo
+{
+    public class SpotifySearch
+    {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        #endregion
+
+        private DataModel m_DataModel = null;
+        private DateNormalizer m_DateNormalizer = null;
+
+        public SpotifySearch(DataModel dataModel)
+        {
+            m_DataModel = dataModel;
+            string[] dateFormat = { "YYYY" };
+            m_DateNormalizer = new DateNormalizer(dateFormat);
+            SearchResults = new ObservableCollection<Playable>();
+        }
+
+        public void Search(string what)
+        {
+            SearchResults.Clear();
+            ServerResponse response = Protocol.Search(m_DataModel.ServerConnection, "any", what);
+
+            IList<PlaylistItem> items = new List<PlaylistItem>();
+            Utils.ParseSongListResponse(response, m_DateNormalizer, items);
+
+            foreach (PlaylistItem item in items)
+            {
+                SongMetadata song = item.Playable as SongMetadata;
+
+                if (song.IsSpotify)
+                {
+                    SearchResults.Add(song);
+                }
+            }
+        }
+
+        public IList<Playable> SearchResults
+        {
+            get;
+            private set;
+        }
+    }
+}
