61283633b6c229e5715e0a8cacfdd89d2705432e
auremompd@gmail.com <auremompd@gmail.com@aed9fa71-8930-dd05-51b1-77e14e162102>
2013-10-13 14:40:10 +0000

    Add user adjustable network timeout option. Make reconnect interval option actually work.

--- Auremo/Auremo/Properties/Settings.Designer.cs
+++ Auremo/Auremo/Properties/Settings.Designer.cs
@@ -303,7 +303,7 @@ namespace Auremo.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("5000")]
+        [global::System.Configuration.DefaultSettingValueAttribute("10")]
         public int NetworkTimeout {
             get {
                 return ((int)(this["NetworkTimeout"]));
