29a00e12ac4d81ce1f02d82889fae4bf1d4e4c90
auremompd <auremompd@aed9fa71-8930-dd05-51b1-77e14e162102>
2014-03-21 16:16:18 +0000

    Hide "show in" menu items when the related tabs are hidden.

--- branches/0.7-dev/Auremo/Auremo/ConverterClasses.cs
+++ branches/0.7-dev/Auremo/Auremo/ConverterClasses.cs
@@ -20,6 +20,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Auremo
@@ -80,6 +81,20 @@ namespace Auremo
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return !(bool)value;
+        }
+    }
+
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (Visibility)value == Visibility.Visible;
         }
     }
 
