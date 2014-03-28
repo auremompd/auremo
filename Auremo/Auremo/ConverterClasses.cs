dd6219a41b3bcb95c70f29d65f9ef1df393cefc9
auremompd <auremompd@aed9fa71-8930-dd05-51b1-77e14e162102>
2014-03-28 13:54:38 +0000

    Don't show two sequential menu separators when all show in... items are collapsed.

--- Auremo/Auremo/ConverterClasses.cs
+++ Auremo/Auremo/ConverterClasses.cs
@@ -118,4 +118,25 @@ namespace Auremo
             return null;
         }
     }
+
+    public class MultiBoolToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (object value in values)
+            {
+                if ((bool)value)
+                {
+                    return Visibility.Visible;
+                }
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
 }
