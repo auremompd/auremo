39bc6041aad8144aff66fdacc946b07646b4a3b0
auremompd <auremompd@aed9fa71-8930-dd05-51b1-77e14e162102>
2014-03-30 11:26:49 +0000

    Optimize SelectVisibleWithinRange() slightly.

--- branches/0.7-dev/Auremo/Auremo/TreeViewController.cs
+++ branches/0.7-dev/Auremo/Auremo/TreeViewController.cs
@@ -80,13 +80,13 @@ namespace Auremo
 
         private void SelectVisibleWithinRange(TreeViewNode node, int minID, int maxID)
         {
-            // TODO: there is plenty left to optimize here.
+            // TODO: optimize more.
             if (minID <= node.ID && node.ID <= maxID)
             {
                 node.IsMultiSelected = true;
             }
 
-            if (node.IsExpanded)
+            if (node.IsExpanded && node.ID <= maxID && node.HighestChildID >= minID)
             {
                 foreach (TreeViewNode child in node.Children)
                 {
