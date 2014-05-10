af909a88a0175214de6be7ace405cb9905a31cc4
auremompd@gmail.com <auremompd@gmail.com@aed9fa71-8930-dd05-51b1-77e14e162102>
2014-05-10 12:38:12 +0000

    Re-branch 0.7-dev to 0.7-gui-dev.

---
+++ branches/0.7-gui-dev/Auremo/Auremo/MainWindowOverlay.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Auremo
+{
+    /// <summary>
+    /// Interaction logic for MainWindowOverlay.xaml
+    /// </summary>
+    public partial class MainWindowOverlay : UserControl, INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        #endregion
+
+        public delegate void OverlayClosingCallback(bool ok, string input);
+
+        private bool m_Active = false;
+        private string m_Caption = "";
+        private string m_Input = "";
+
+        public MainWindowOverlay()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Callback = null;
+            Data = null;
+        }
+
+        public void Activate(string caption, string defaultInput, OverlayClosingCallback callback, object data = null)
+        {
+            Caption = caption;
+            Input = defaultInput;
+            Callback = callback;
+            Data = data;
+            Active = true;
+            
+        }
+
+        public void Deactivate()
+        {
+            Active = false;
+            Caption = "";
+            Input = "";
+            Callback = null;
+            Data = null;
+        }
+
+        public bool Active
+        {
+            get
+            {
+                return m_Active;
+            }
+            set
+            {
+                if (value != m_Active)
+                {
+                    m_Active = value;
+                    Visibility = m_Active ? Visibility.Visible : Visibility.Collapsed;
+                    NotifyPropertyChanged("Active");
+                }
+            }
+        }
+
+        public string Caption
+        {
+            get
+            {
+                return m_Caption;
+            }
+            set
+            {
+                if (value != m_Caption)
+                {
+                    m_Caption = value;
+                    NotifyPropertyChanged("Caption");
+                }
+            }
+        }
+
+        public string Input
+        {
+            get
+            {
+                return m_Input;
+            }
+            set
+            {
+                if (value != m_Input)
+                {
+                    m_Input = value;
+                    NotifyPropertyChanged("Input");
+                }
+            }
+        }
+
+        public OverlayClosingCallback Callback
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Extra data that the caller can store for this query.
+        /// </summary>
+        public object Data
+        {
+            get;
+            set;
+        }
+
+        private void OnOK(object sender, RoutedEventArgs e)
+        {
+            if (Callback == null)
+            {
+                Active = false;
+            }
+            else
+            {
+                Callback(true, Input);
+            }
+        }
+
+        private void OnCancel(object sender, RoutedEventArgs e)
+        {
+            if (Callback == null)
+            {
+                Active = false;
+            }
+            else
+            {
+                Callback(false, Input);
+            }
+        }
+
+        private void OnBackgroundClicked(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void OnVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (IsVisible)
+            {
+                Keyboard.Focus(m_InputBox);
+                m_InputBox.CaretIndex = m_InputBox.Text.Length;
+            }
+        }
+    }
+}
